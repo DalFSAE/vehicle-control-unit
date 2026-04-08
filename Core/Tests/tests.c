67c68276a661203e2db94c6de132cc7e09e4d1c5
Ashton Dudley <ashtondudley03@gmail.com>
2026-04-08 16:21:19 -0300

    updated tests

--- Core/Tests/tests.c
+++ Core/Tests/tests.c
@@ -2,30 +2,30 @@
 
 #include "cmsis_os2.h"
 #include "io_control.h"
-#include "relay.h"
+#include "board_outputs.h"
 
 
 
 void test_relays(void) {
-    uint32_t stateRelay0 = relay_get_state(RELAY_ALWAYS_ON);
-    uint32_t stateRelay1 = relay_get_state(RELAY_BRAKE_LIGHT);
-    uint32_t stateRelay2 = relay_get_state(RELAY_INVERTER);
-    uint32_t stateRelay3 = relay_get_state(RELAY_FANS);
-    uint32_t stateRelay4 = relay_get_state(RELAY_SDC);
+    uint32_t stateRelay0 = board_output_get_state(OUTPUT_ALWAYS_ON);
+    uint32_t stateRelay1 = board_output_get_state(OUTPUT_BRAKE_LIGHT);
+    uint32_t stateRelay2 = board_output_get_state(OUTPUT_INVERTER);
+    uint32_t stateRelay3 = board_output_get_state(OUTPUT_FANS);
+    uint32_t stateRelay4 = board_output_get_state(OUTPUT_SDC);
     
-    relay_toggle(RELAY_ALWAYS_ON);
+    board_output_toggle(OUTPUT_ALWAYS_ON);
     osDelay(50);
-    relay_toggle(RELAY_BRAKE_LIGHT);
+    board_output_toggle(OUTPUT_BRAKE_LIGHT);
     osDelay(50);
-    relay_toggle(RELAY_INVERTER);
+    board_output_toggle(OUTPUT_INVERTER);
     osDelay(50);
-    relay_toggle(RELAY_FANS);
+    board_output_toggle(OUTPUT_FANS);
     osDelay(50);
-    relay_toggle(RELAY_SDC);
+    board_output_toggle(OUTPUT_SDC);
     
-    stateRelay0 = relay_get_state(RELAY_ALWAYS_ON);
-    stateRelay1 = relay_get_state(RELAY_BRAKE_LIGHT);
-    stateRelay2 = relay_get_state(RELAY_INVERTER);
-    stateRelay3 = relay_get_state(RELAY_FANS);
-    stateRelay4 = relay_get_state(RELAY_SDC);
+    stateRelay0 = board_output_get_state(OUTPUT_ALWAYS_ON);
+    stateRelay1 = board_output_get_state(OUTPUT_BRAKE_LIGHT);
+    stateRelay2 = board_output_get_state(OUTPUT_INVERTER);
+    stateRelay3 = board_output_get_state(OUTPUT_FANS);
+    stateRelay4 = board_output_get_state(OUTPUT_SDC);
 }
