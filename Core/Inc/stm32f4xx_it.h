65ac6d5b2a686c37948e1dd46035d6634c98ade1
Lorne MacLean <161739574+Lorne-Mac@users.noreply.github.com>
2026-04-27 01:42:12 -0300

    Switched to new .ioc file (#39)
    * Changed out ioc file for the new one
    
    * Updated ioc file to include an analog output pin
    
    * Updated IOC file to new version
    
    * Hw 3.0 add drivers (#42)
    
    * Added drivers generated by CubeMX
    
    * Added middleware generated by CubeMX
    
    * Added generated .c/.h files
    
    * Updated .ioc file and generated code
    
    * Added FreeRTOS to .ioc
    
    * VSCode Settings
    
    ---------
    
    Co-authored-by: Ashton Dudley <ashtondudley03@gmail.com>

--- Core/Inc/stm32f4xx_it.h
+++ Core/Inc/stm32f4xx_it.h
@@ -22,7 +22,7 @@
 #define __STM32F4xx_IT_H
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 /* Private includes ----------------------------------------------------------*/
@@ -53,7 +53,6 @@ void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void DebugMon_Handler(void);
 void TIM6_DAC_IRQHandler(void);
-void DMA2_Stream0_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
