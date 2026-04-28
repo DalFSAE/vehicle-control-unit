4036c3a710840ec1ac3cfac8d787fb8760fe60c6
Ashton Dudley <ashtondudley03@gmail.com>
2026-04-27 21:35:42 -0300

    fixed build

--- Core/User/torque_output.h
+++ Core/User/torque_output.h
@@ -1,15 +1,16 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stdint.h>
 
 typedef struct {
     bool       inv_enable;
     bool       inv_discharge;
     bool       speed_mode_enable;
-    MotorDir_t direction;
+    bool       motor_direction_forward;
     float      torque_command_nm;
     float      torque_limit_nm;
-    int16_t    speed_command_rpm;
+    uint16_t    speed_command_rpm;
 } VcuInverterCommand_t;
 
 // Update torque output. Routes to DAC now; swap implementation for CAN later.
