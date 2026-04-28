c1a56df097e8c46067bc2c04828aca786a1fd09f
Ashton Dudley <ashtondudley03@gmail.com>
2026-04-28 01:11:18 -0300

    Updated runner

---
+++ Core/Tests/unit/runner_fsm.c
@@ -0,0 +1,6 @@
+#include "unity.h"
+#include "test_fsm.h"
+
+int main(void) {
+    return run_fsm_tests();
+}
--- Core/Tests/unit/stubs.c
+++ Core/Tests/unit/stubs.c
@@ -1,10 +1,6 @@
 #include "log.h"
-#include "unity/unity.h"
 
 bool log_write(const LogEvent_t *event) {
     (void)event;
     return true;
 }
-
-void setUp(void) {}
-void tearDown(void) {}
