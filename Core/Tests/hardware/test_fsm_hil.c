485d640f583ed3b95fba554b7b24d4fbf50d907f
Ashton Dudley <adudley@dal.ca>
2026-05-03 00:18:43 -0300

    Add motor controller CAN loopback test (#68)
    * Added ignore condition to button test
    
    * Improved test interface
    
    * Improvements to MC logging
    
    * Working loopback test
    
    * Created `fsm_test_helpers.*`
    
    * Updated tests
    
    * Added tests and fixed throttle request

--- Core/Tests/hardware/test_fsm_hil.c
+++ Core/Tests/hardware/test_fsm_hil.c
@@ -1,4 +1,5 @@
 #include "test_fsm_hil.h"
+#include "fsm_test_helpers.h"
 #include "input_control.h"
 #include "unity.h"
 #include "fsm.h"
@@ -6,47 +7,8 @@
 #include "vehicle_state.h"
 #include "vcu_io.h"
 #include "board_outputs.h"
-#include "sensor_control.h"
 #include "cmsis_os2.h"
 #include "dio.h"
-
-#include <string.h>
-
-#define FSM_SETTLE_MS (FSM_PERIOD_MS * 4u)
-
-// ===========================================================================
-// Helpers
-// ===========================================================================
-
-static void suspend_sensor(void) { osThreadSuspend(sensor_task_get_handle()); }
-static void resume_sensor(void)  { osThreadResume(sensor_task_get_handle()); }
-
-static VcuInputs s_spoof = {0};
-
-static void clear_inputs(void) {
-    memset(&s_spoof, 0, sizeof(s_spoof));
-    vcu_spoof_inputs(&s_spoof);
-}
-
-// Drive FSM from STANDBY to NEUTRAL. Assumes sensor is already suspended.
-static void walk_to_neutral(void) {
-    clear_inputs();
-    s_spoof.fwrd_switch = true;
-    s_spoof.ts_active   = true;
-    vcu_spoof_inputs(&s_spoof);
-    osDelay(FSM_SETTLE_MS);
-}
-
-// Drive FSM from NEUTRAL to FORWARD.
-static void walk_to_forward(void) {
-    s_spoof.fwrd_switch  = true;
-    s_spoof.brake_pressed = true;
-    s_spoof.rtd_button   = true;
-    vcu_spoof_inputs(&s_spoof);
-    osDelay(FSM_SETTLE_MS);
-    s_spoof.rtd_button = false;
-    vcu_spoof_inputs(&s_spoof);
-}
 
 // ===========================================================================
 // Tests
@@ -60,16 +22,16 @@ void test_fsm_standby_requires_switch_and_ts(void) {
     suspend_sensor();
 
     clear_inputs();
-    s_spoof.fwrd_switch = true;
-    s_spoof.ts_active   = false;
-    vcu_spoof_inputs(&s_spoof);
+    g_spoof.fwrd_switch = true;
+    g_spoof.ts_active   = false;
+    vcu_spoof_inputs(&g_spoof);
     osDelay(FSM_SETTLE_MS);
     TEST_ASSERT_EQUAL(ST_STANDBY, g_fsm_state);
 
     clear_inputs();
-    s_spoof.fwrd_switch = false;
-    s_spoof.ts_active   = true;
-    vcu_spoof_inputs(&s_spoof);
+    g_spoof.fwrd_switch = false;
+    g_spoof.ts_active   = true;
+    vcu_spoof_inputs(&g_spoof);
     osDelay(FSM_SETTLE_MS);
     TEST_ASSERT_EQUAL(ST_STANDBY, g_fsm_state);
 
@@ -92,13 +54,13 @@ void test_fsm_rtd_requires_brake(void) {
     suspend_sensor();
     walk_to_neutral();
 
-    s_spoof.fwrd_switch  = true;
-    s_spoof.brake_pressed = false;
-    s_spoof.rtd_button   = true;
-    vcu_spoof_inputs(&s_spoof);
+    g_spoof.fwrd_switch   = true;
+    g_spoof.brake_pressed = false;
+    g_spoof.rtd_button    = true;
+    vcu_spoof_inputs(&g_spoof);
     osDelay(FSM_SETTLE_MS);
-    s_spoof.rtd_button = false;
-    vcu_spoof_inputs(&s_spoof);
+    g_spoof.rtd_button = false;
+    vcu_spoof_inputs(&g_spoof);
 
     TEST_ASSERT_EQUAL(ST_NEUTRAL, g_fsm_state);
 
@@ -122,9 +84,9 @@ void test_fsm_pedal_plaus_returns_to_neutral(void) {
     walk_to_neutral();
     walk_to_forward();
 
-    s_spoof.fwrd_switch  = true;
-    s_spoof.fault_flags  = FAULT_PEDAL_PLAUS;
-    vcu_spoof_inputs(&s_spoof);
+    g_spoof.fwrd_switch = true;
+    g_spoof.fault_flags = FAULT_PEDAL_PLAUS;
+    vcu_spoof_inputs(&g_spoof);
     osDelay(FSM_SETTLE_MS);
 
     TEST_ASSERT_EQUAL(ST_NEUTRAL, g_fsm_state);
@@ -138,18 +100,25 @@ void test_if_debug_button_changes_state(void) {
     suspend_sensor();
     walk_to_neutral();
 
-    s_spoof.fwrd_switch  = true;
-    s_spoof.brake_pressed = true;
+    g_spoof.fwrd_switch   = true;
+    g_spoof.brake_pressed = true;
+    bool button_was_pressed = false;
 
     for (int i = 0; i < 20; i++) {
-        s_spoof.rtd_button = read_pcb_user_button();
-        vcu_spoof_inputs(&s_spoof);
+        g_spoof.rtd_button = read_pcb_user_button();
+        button_was_pressed |= g_spoof.rtd_button;
+        vcu_spoof_inputs(&g_spoof);
         osDelay(100);
-        if (g_fsm_state == ST_FORWARD) break;
+        if (g_fsm_state == ST_FORWARD)
+            break;
     }
 
-    TEST_ASSERT_EQUAL_MESSAGE(ST_FORWARD, g_fsm_state,
-        "RTD button was not pressed during the test window");
+    if (!button_was_pressed) {
+        TEST_IGNORE_MESSAGE("RTD button was not pressed during the test window");
+    } else {
+        TEST_ASSERT_EQUAL_MESSAGE(ST_FORWARD, g_fsm_state,
+            "FSM did not transition to FORWARD state after RTD button was pressed");
+    }
 
     clear_inputs();
     resume_sensor();
@@ -159,23 +128,27 @@ void test_throttle_step_response_from_nuetral(void) {
     suspend_sensor();
     walk_to_neutral();
 
-    s_spoof.fwrd_switch  = false;
-    s_spoof.brake_pressed = false;
-    s_spoof.rtd_button   = true;
-    vcu_spoof_inputs(&s_spoof);
+    g_spoof.fwrd_switch   = false;
+    g_spoof.brake_pressed = false;
+    g_spoof.rtd_button    = true;
+    vcu_spoof_inputs(&g_spoof);
     osDelay(FSM_SETTLE_MS);
-    s_spoof.rtd_button = false;
+    g_spoof.rtd_button = false;
 
-    for (int i = 0; i <= 10; i++) {
-        s_spoof.throttle_request = i / 10.0f;
-        vcu_spoof_inputs(&s_spoof);
-        osDelay(FSM_SETTLE_MS);
-    }
-    for (int i = 9; i >= 0; i--) {
-        s_spoof.throttle_request = i / 10.0f;
-        vcu_spoof_inputs(&s_spoof);
-        osDelay(FSM_SETTLE_MS);
-    }
+    ramp_throttle(0.0f, 1.0f, 10, FSM_SETTLE_MS);
+    ramp_throttle(1.0f, 0.0f, 10, FSM_SETTLE_MS);
+
+    clear_inputs();
+    resume_sensor();
+}
+
+void test_throttle_step_response_from_forward(void) {
+    suspend_sensor();
+    walk_to_neutral();
+    walk_to_forward();
+
+    ramp_throttle(0.0f, 1.0f, 10, FSM_SETTLE_MS);
+    ramp_throttle(1.0f, 0.0f, 10, FSM_SETTLE_MS);
 
     clear_inputs();
     resume_sensor();
