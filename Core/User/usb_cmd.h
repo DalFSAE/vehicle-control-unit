b5d16d0d93b14ccaf80c9f86111935f586a69df7
Ashton Dudley <adudley@dal.ca>
2026-06-02 16:58:02 -0300

    Made enum public

--- Core/User/usb_cmd.h
+++ Core/User/usb_cmd.h
@@ -5,4 +5,15 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+typedef enum {
+    CMD_SPOOF_SET       = 0x01,
+    CMD_SPOOF_CLEAR     = 0x02,
+    CMD_REQUEST_OUTPUTS = 0x03,
+    CMD_REQUEST_STATE   = 0x04,
+    CMD_STEP            = 0x05,
+    CMD_FAULT_INJECT    = 0x06,
+    CMD_RESET           = 0x07,
+    CMD_ECHO            = 0x45,
+} UsbCmd_t;
+
 uint32_t usb_cmd_rx(const uint8_t *buf, uint32_t len);
