02271b13cf47b86854df71e27c8a2c640a1260db
reubenchapple <reubschapple@gmail.com>
2026-06-11 23:03:59 -0300

    updated torque_processing.c and header file to conform to the feedback on the pull request. I dont know how to use clangd, so i did not run that.

---
+++ Core/User/torque_processing.h
@@ -0,0 +1,28 @@
+#include <stdbool.h>
+#include "sensor_types.h"
+//struct containing configurables that will impact the torque algorithm calculation
+//these variables were selected via the torque parameters graph on page 53 of https://github.com/DalFSAE/motor-controller-fw/blob/main/docs/Software%20User%20Manual%20(V3_6)%20(1).pdf
+//regenerative torque is assumed to be negative.
+typedef struct{
+    float peddle_lo; //deadzone, ignore anything below this value, e.g. 0.05 -> ignore below 5%
+    float peddle_hi; //deadzone, ignore anything above this value, e.g. 0.95 -> ignore above 95%
+    float accel_min; //regen approaches 0 from here
+    float accel_max; //torque output = max torque here
+    float coast_lo; //regen = 0 here
+    float coast_hi; //torque output > 0 from here
+    float motor_torque_limit; // physical limit of torque output
+    float regen_torque_limit; //maximum torque regen
+} motor_torque_config_t;
+
+void motor_torque_init(motor_torque_config_t cfg);
+float motor_torque(SensorInfo_t sensor);
+
+//this enum represents all the possible torque output states
+typedef enum {
+    TORQUE_STATE_ERROR,
+    TORQUE_STATE_REGEN_FULL,
+    TORQUE_STATE_REGEN_RAMP,
+    TORQUE_STATE_COAST,
+    TORQUE_STATE_ACCEL_RAMP,
+    TORQUE_STATE_ACCEL_FULL,
+} torque_state_t;
